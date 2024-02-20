888993dc213a18c1f4d091de1308af0a4e318ec0
Ayan Maiti <mightyayan369@gmail.com>
2024-02-20 20:42:47 +0530

    Add configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.24",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
     sepolia: {
       url: "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       accounts: [process.env.PRIVATE_KEY_S],
